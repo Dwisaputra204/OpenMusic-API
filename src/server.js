3949a864cac36f56181c9143da386150dd295892
Dwisaputra204 <dwisaputra204@gmail.com>
2021-07-27 20:05:48 +0700

    submission 2

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-unresolved */
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 const songs = require('./api/songs');
 const SongsService = require('./services/postgres/SongsService');
 const SongsValidator = require('./validator/songs');
@@ -26,6 +27,28 @@ const init = async () => {
         origin: ['*'],
       },
     },
+  });
+
+  await server.register([
+    {
+      plugin: Jwt,
+    },
+  ]);
+
+  server.auth.strategy('openmusic_jwt', 'jwt', {
+    keys: process.env.ACCESS_TOKEN_KEY,
+    verify: {
+      aud: false,
+      iss: false,
+      sub: false,
+      maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+    },
+    validate: (artifacts) => ({
+      isValid: true,
+      credentials: {
+        id: artifacts.decoded.payload.id,
+      },
+    }),
   });
 
   await server.register([
---
+++ src/validator/playlists/schema.js
@@ -0,0 +1,23 @@
+/* eslint-disable import/no-self-import */
+const {
+  PostPlaylistPayloadSchema,
+  PostSongPayloadSchema,
+} = require('./schema');
+const InvariantError = require('../../exceptions/InvariantError');
+
+const PlaylistsValidator = {
+  validatePostPlaylistPayload: (payload) => {
+    const validationResult = PostPlaylistPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+  validatePostSongPayload: (payload) => {
+    const validationResult = PostSongPayloadSchema.validate(payload);
+    if (validationResult.error) {
+      throw new InvariantError(validationResult.error.message);
+    }
+  },
+};
+
+module.exports = PlaylistsValidator;
